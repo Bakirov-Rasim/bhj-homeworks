a3071f9d27c98bbc4774e64469b405a0512b326c
Bakirov-Rasim <bakir08@yandex.ru>
2020-01-16 21:18:54 +0300

    Добавил решение

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -2,5 +2,10 @@ const image = document.getElementById("cookie");
 const clicker = document.getElementById("clicker__counter");
 function clicks() {
     console.log(clicker.textContent++);
+    if (clicker.textContent % 2 > 0) {
+        image.width = image.width / 2;
+    } else {
+        image.width = image.width * 2;
+    }
 };
 image.onclick = clicks;
