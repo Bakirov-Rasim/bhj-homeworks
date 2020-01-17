916c01ab58b55fbd68a8d583a2818bace699cc5d
Bakirov-Rasim <Bakir08@yandex.ru>
2020-01-17 15:45:07 +0300

    не доделано

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,26 @@
+const killed = document.getElementById("dead");
+const missed = document.getElementById("lost");
+function clicks() {
+    if (this.className.includes("hole_has-mole")) {
+        killed.textContent ++;
+    }
+    else {
+        missed.textContent ++;
+    }
+
+    if (killed.textContent > 10) {
+        alert("Вы победили");
+    }
+    else if (missed.textContent > 5) {
+        alert("Вы проиграли");
+    }
+}
+hole1.onclick = clicks;
+hole2.onclick = clicks;
+hole3.onclick = clicks;
+hole4.onclick = clicks;
+hole5.onclick = clicks;
+hole6.onclick = clicks;
+hole7.onclick = clicks;
+hole8.onclick = clicks;
+hole9.onclick = clicks;
