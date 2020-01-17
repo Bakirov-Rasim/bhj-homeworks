75f956f3dfbefd3eb69ac1270736b07e8b614a35
Bakirov-Rasim <Bakir08@yandex.ru>
2020-01-17 06:48:16 +0300

    Update task.js
    111

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,9 @@
+const image = document.getElementById("cookie");
+const pieClick = document.getElementById("clicker__counter");
+
+function pieClicker() {
+    image.width = 1000;
+    image.height = 1000;
+}
+
+image onclick = pieClicker;
